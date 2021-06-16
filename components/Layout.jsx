3524773b8e28411f9b2b297fd7fea20695f689e4
Joshuatauro <joshuatauro45@gmail.com>
2021-06-16 11:28:45 +0400

    Added under development banner

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,6 +3,11 @@ import Navbar from './Navbar'
 const Layout = ({children}) => {
   return (
     <div className="font-inter">
+      <div className="w-full bg-black text-white">
+        <h1 className="text-sm py-2 flex justify-center">   
+          This site is currently under development
+        </h1>
+      </div>
       <Navbar />
       {children}
     </div>
